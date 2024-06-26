1384caf4d6ef8fe815e49ef64343460136fcafd5
Hao <a840817a@yahoo.com.tw>
2024-06-26 16:14:38 +0800

    add Dockerfile

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import {IConfig} from "./definition/interface/config";
 
 @injectable()
 export class Config implements IConfig {
-    readonly OUTPUT_PATH = 'temp/';
+    readonly OUTPUT_PATH = 'archive/';
     readonly MAX_ERROR_RETRY = 3;
-    readonly MAX_CONCURRENT_DOWNLOADS = 15;
+    readonly MAX_CONCURRENT_DOWNLOADS = 16;
 }
